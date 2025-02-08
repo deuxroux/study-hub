8d6d99cb7865675a760b34f02139dec6d1820693
Vineet <erasalav@gmail.com>
2025-02-08 17:03:04 -0500

    added initial logic for user authentication and registration, login/out and additional html for page rendering

--- study_hub_app/study_hub/views.py
+++ study_hub_app/study_hub/views.py
@@ -1,5 +1,61 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from .models import *
+from .forms import *
 
 # Create your views here.
 def index(request):
     return render(request, 'study_hub/index.html')
+
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect('/')
+
+
+def user_login(request):
+    if request.method == 'POST':
+
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect('/')
+            else:
+                return HttpResponse("Your account is disabled.")
+        else:
+            return HttpResponse("Invalid login details supplied.")
+    else:
+        return render(request, 'study_hub/login.html', )
+
+
+def register(request):
+    registered = False
+
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileForm(data=request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            # if 'is_teacher' in user_form.cleaned_data:
+            #     profile.is_teacher = request.DATA['is_teacher']
+            # profile.save()
+            # # return redirect('/login')
+            registered = True
+        else:
+            print(user_form.errors, profile_form.errors)
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+
+    return render(request, 'study_hub/register.html', {'user_form': user_form,
+                                                       'profile_form': profile_form,
+                                                       'registered': registered})
--- study_hub_app/study_hub_app/settings.py
+++ study_hub_app/study_hub_app/settings.py
@@ -120,6 +120,9 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+AUTH_USER_MODEL = 'study_hub.User'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
